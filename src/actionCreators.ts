d6836458900e1c5741d6e697b0994bd58d4a073e
George <eakarpov@yandex.ru>
2017-10-22 20:29:16 +0300

    void to Action type

--- src/actionCreators.ts
+++ src/actionCreators.ts
@@ -1,12 +1,12 @@
-import {IStudentAction} from "./types";
+import {Action, IStudentAction} from "./types";
 import {ADD_ACTION} from "./actions";
 import Notifier from "./Notifier";
 
-export const addAction = (payload: IStudentAction) => {
-  Notifier.send(payload.message, payload.fee).then(() => {
+export const addAction = (payload: IStudentAction): Action => {
+  return Notifier.send(payload.message, payload.fee).then(() => {
     return {
       type: ADD_ACTION,
       payload,
-    } as IStudentAction;
+    } as Action;
   });
 };
