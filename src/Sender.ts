5296c0b6e6ef1df393f4cf99984c59b549c96755
eakarpov <eakarpov@yandex.ru>
2018-12-13 01:34:14 +0300

    type fixes

--- src/Sender.ts
+++ src/Sender.ts
@@ -1,16 +1,22 @@
 import {IResult, IPrepared} from "./types";
-import axios, {AxiosResponse} from "axios";
 import {getConfig} from "./config";
 
 export default class Sender {
-  async send(messageObject: IPrepared): Promise<IResult> {
-    const config = getConfig();
-    return axios.post(`${config.protocol}://${config.host}:${config.port}/${config.path}`, messageObject)
-      .then((response: AxiosResponse<any>) => {
-      return {
-        status: response.status,
-        message: response.data.message
-      } as IResult;
-    });
-  }
+    private status?: number;
+    async send(messageObject: IPrepared): Promise<IResult> {
+        const config = getConfig();
+        return fetch(`${config.protocol}://${config.host}:${config.port}/${config.path}`, {
+            body: JSON.stringify(messageObject),
+            method: 'post',
+        })
+            .then((response: Response) => {
+                this.status = response.status;
+                return response.json();
+            }).then(data => {
+                return {
+                    status: this.status,
+                    message: data.message
+                } as IResult;
+            });
+    }
 }
--- src/reducer.ts
+++ src/reducer.ts
@@ -1,3 +1,5 @@
+// DO NOT REMOVE THIS IMPORT!
+import {INotifierStore,Action}  from "./types";
 import createReducer from "./createReducer";
 
 const reducer = createReducer();
